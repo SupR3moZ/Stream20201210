7ddebd964caefdfffc023cd427e421dd5f49b3fc
Nikita Kolovanov <89263219040@ya.ru>
2020-12-17 12:53:11 +0300

    Сделал 2 задачу из дз

---
+++ src/lesson01/online/homework/BooleanCheck.java
@@ -0,0 +1,31 @@
+package lesson01.online.homework;
+
+public class BooleanCheck {
+    public static void main(String[] args){
+        int a = 16;
+        int b = 5;
+
+        if ((((a + b) <= 20)) && ((a + b) >= 10)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+
+
+//        public static int result(int valueA, int valueB){
+//        return valueA + valueB;
+//        }
+
+    }
+}
+//            public static boolean trueFalse (int checkValue) {
+//                if (20 >= checkValue() && checkValue() => 10);
+//            }
+
+/*
+2) Написать метод, принимающий на вход два целых числа,
+и проверяющий что их сумма лежит в пределах от 10 до 20(включительно),
+если да – вернуть true, в противном случае – false;
+
+ */
