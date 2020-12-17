ffd33067e36778e9cc698592cce26ea997c64029
Nikita Kolovanov <89263219040@ya.ru>
2020-12-17 11:36:10 +0300

    Сделал 1 пункт дз по уроку 1 в классе EquationResult.java

---
+++ src/lesson01/online/homework/EquationResult.java
@@ -0,0 +1,21 @@
+package lesson01.online.homework;
+
+public class EquationResult {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 100;
+        int c = 1000;
+        int d = 10000;
+
+        System.out.println(equation(a, b, c, d));
+    }
+        //создадим метод, который будет возвращать результат уравнения
+        public static float equation (int valueA, int valueB, float valueC,float valueD){
+        return (valueA * (valueB +(valueC / valueD)));
+    }
+        /*Интересно, что если в аргументах метода указать для значений valueC и valueD тип int - Idea ругается но возвращает результат который является неверным.
+        10 * (100 + (1000 / 10000) = 1001 верно, однако если указать для вышеупомянутых значений тип int в консоль выводится результат 1000 - неверно
+         */
+}
+
